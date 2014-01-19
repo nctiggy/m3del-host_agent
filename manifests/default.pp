cba5817af025f24b928373f7f9358e96c31bb11b
Craig J Smith <nctiggy@gmail.com>
2014-01-19 15:00:36 -0800

    Installer complete
    This is for centos

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,10 +1,16 @@
-package { '/vagrant/files/HostAgent-Linux-32-x86-en_US-1.3.1.1.0033-1.i386.rpm':
+package { 'HostAgent-Linux-32-x86-en_US-1.3.1.1.0033-1.i386.rpm':
 	ensure => installed,
 	provider => 'rpm',
+	#install_options => '-vh',
 	source => '/vagrant/files/HostAgent-Linux-32-x86-en_US-1.3.1.1.0033-1.i386.rpm',
 	require => Package['glibc'],
 }
 
 package { 'glibc':
-	ensure => 'installed',
+	ensure => installed,
+	require => Exec['/usr/bin/yum check-update'],
+}
+
+exec { '/usr/bin/yum check-update':
+	returns => 100
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,11 @@ class host_agent::install inherits host_agent {
 	package { '/vagrant/files/HostAgent-Linux-32-x86-en_US-1.3.1.1.0033-1.i386.rpm':
 		ensure => installed,
 		provider => 'rpm',
-		require => File['/tmp/HostAgent-Linux-32-x86-en_US-1.3.1.1.0033-1.i386.rpm']
+		require => Package['libm.so.6']
+	}
+	
+	package { 'libm.so.6':
+		ensure => 'installed',
 	}
 	
 }
