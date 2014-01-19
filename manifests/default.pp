9379f15c6e10f68d14bf6acd9d2dfbb9ba40bab5
Craig J Smith <nctiggy@gmail.com>
2014-01-19 15:17:30 -0800

    Install fixed
    glibc-2.12-1.132.el6.i686 is the correct pre-req

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,10 +3,10 @@ package { 'HostAgent-Linux-32-x86-en_US-1.3.1.1.0033-1.i386.rpm':
 	provider => 'rpm',
 	#install_options => '-vh',
 	source => '/vagrant/files/HostAgent-Linux-32-x86-en_US-1.3.1.1.0033-1.i386.rpm',
-	require => Package['glibc'],
+	require => Package['glibc-2.12-1.132.el6.i686'],
 }
 
-package { 'glibc':
+package { 'glibc-2.12-1.132.el6.i686':
 	ensure => installed,
 	require => Exec['/usr/bin/yum check-update'],
 }
